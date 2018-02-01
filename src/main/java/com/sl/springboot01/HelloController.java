d980372f3d5ae77bd1ec03e34eb9306594b9326d
shenliang00 <872556035@qq.com>
2018-02-01 10:59:34 +0800

    第一个SpringBoot测试

---
+++ src/main/java/com/sl/springboot01/HelloController.java
@@ -0,0 +1,12 @@
+package com.sl.springboot01;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+       @RequestMapping("/hello")
+       public String hello(){
+           return "hello,this is";
+       }
+}
